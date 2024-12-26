bfade2bafe1815e0c511deaddb1289b1ba2f178c
yuliatik <166557649+yuliatik@users.noreply.github.com>
2024-12-26 23:15:14 +0100

    Footer.jsx

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -3,21 +3,39 @@ import React from 'react';
 import './Footer.css'; 
 
 
-
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <p>© 2024 Aura Balance. Alle Rechte vorbehalten.</p>
+
+        <div>
+          <h4>Kontakt:</h4>
+          <p>Steinstraße 4</p>
+          <p>22529 Hamburg</p>
+          <p>E-mail: kontakt@aura-balance.com</p>
+          <p>Telefon: +49 174 5962835</p>
+        </div>
+
+        <div>
+        <h4>Unsere Öffnungszeiten</h4>
+         <ul className="opening-hours">
+          <li>Montag - Freitag: 10:00 - 21:00</li>
+          <li>Samstag: 10:00 - 16:00</li>
+          <li>Sonntag: Geschlossen</li>
+         </ul>
+         </div>         
+
+         <p>© 2024 Aura Balance. Alle Rechte vorbehalten.</p>
+
         <p>Entwickelt von yuliatik für die Lernzwecke.</p>
         <ul className="footer-links">
           <li><a href="#about">Über uns</a></li>
           <li><a href="#services">Services</a></li>
           <li><a href="#contact">Kontakt</a></li>
-        </ul>
-
+        </ul>      
         
      </div>
+     
     </footer>
   );
 };
