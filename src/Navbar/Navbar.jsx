98aaad34b0a120d24fdee23843ababcbdfe469fe
yuliatik <166557649+yuliatik@users.noreply.github.com>
2024-12-26 23:11:57 +0100

    Navbar.jsx

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,34 +1,33 @@
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-    Link
-  } from "react-router-dom";
-import About from './About';
-import Pricing from './Pricing';
-import Contact from './Contact';
-import Home from './Home';
-import './Navbar.css';
+  import React from 'react';
+  import './Navbar.css';
+  import About from './About';
+  import Pricing from './Pricing';
+  import Contact from './Contact';
+  // import Home from './Home';
+  import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
-
-function Navbar() {
-    return  <Router>
-        <nav>
-          <Link to = "/home" className='link'>Startseite</Link>
+  function Navbar() {
+     return <Router>
+       <nav>
+          <Link to = "/" className='link'>Startseite</Link>
           <Link to = "/about" className='link'>Unsere Leistungen</Link>
           <Link to = "/pricing" className='link'>Preise</Link>
           <Link to = "/contact" className='link'>Kontakt</Link>
         </nav>
-  
+
         <Routes>
-          <Route path = "/Contact" element = {<Home/>} />
-          <Route path = "/About" element = {<About/>} />
-          <Route path = "/Pricing" element = {<Pricing/>} />
-          <Route path = "/Contact" element = {<Contact/>} />    
+          {/* <Route path="/" element={<Home />} />  */}
+          <Route path = "/about" element={<About/>}/>
+          <Route path = "/pricing" element = {<Pricing/>} />
+          <Route path = "/contact" element = {<Contact/>} />      
+        </Routes>   
+    </Router> 
+}
   
-        </Routes>      
-      </Router>
-    ;
-  }
-  
+
+  export default Navbar;
+
+
+
+
   export default Navbar;
